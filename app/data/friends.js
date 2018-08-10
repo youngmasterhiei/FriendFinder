a265b62c613ebc372e692a556328e695848b267e
Hiei <james.illius@yahoo.com>
2018-08-10 14:21:20 -0400

    took out testing comments, finished

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 var friendsArray = [
-{
-    "name":"Ahmed",
-    "photo":"https://mybreakfastclub.files.wordpress.com/2009/11/12.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        // 4,
-        // 5,
-        // 1,
-        // 2,
-        // 5,
-        // 4,
-        // 1
-      ]
-  }
+    {
+        "name": "Ahmed",
+        "photo": "https://mybreakfastclub.files.wordpress.com/2009/11/12.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
 ];
 module.exports = friendsArray;
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -28,20 +28,20 @@ module.exports = function (app) {
       name: "",
       photo: "",
       scoreDifference: Infinity
-};
+    };
 
     var newFriend = req.body;
-    
+
     var totalDifference = 0;
 
-    for (var i = 0; i<friendsArray.length; i++) {
-      
+    for (var i = 0; i < friendsArray.length; i++) {
+
       totalDifference = 0;
-      
-      for (var j = 0; j<newFriend.scores.length; j++){
+
+      for (var j = 0; j < newFriend.scores.length; j++) {
         totalDifference += Math.abs(parseInt(newFriend.scores[j]) - parseInt(friendsArray[i].scores[j]));
       }
-      if(totalDifference <= match.scoreDifference){
+      if (totalDifference <= match.scoreDifference) {
         match.name = friendsArray[i].name;
         match.photo = friendsArray[i].photo;
         match.scoreDifference = totalDifference;
